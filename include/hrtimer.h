5e225d0cad6e0bd6de3b85cacd54295012c7549d
Jon <jon.forums@gmail.com>
2011-11-28 12:45:35 -0500

    Initial Linux impl using `clock_gettime()` API

--- include/hrtimer.h
+++ include/hrtimer.h
@@ -1,12 +1,14 @@
 /* Copyright (c) 2011, Jon Maken
  * License: 3-clause BSD
- * Revision: 11/27/2011 7:10:45 PM
+ * Revision: 11/28/2011 12:43:19 PM
  */
 
 // TODO
 // * relook at `SetThreadAffinityMask` set/reset usage in `start` & `stop`
 // * verify default `Timer(Timer& src)` and `void operator=(Timer& src)`
-// * impl Linux hires timing support
+// * impl Linux hires timing support via `clock_gettime(CLOCK_MONOTONIC, &ts)`
+//   and `clock_getres(CLOCK_MONOTONIC, &ts)`
+// * use CLOCK_MONOTONIC or CLOCK_REALTIME for Linux impl?
 
 #ifndef HIRES_TIMER_H_
 #define HIRES_TIMER_H_
@@ -17,6 +19,8 @@
 
 #if defined(_WIN32)
 #  include <windows.h>
+#else
+#  include <time.h>
 #endif
 
 namespace HiRes {
@@ -37,8 +41,13 @@ public:
 
 private:
     double frequency;
+#if defined(_WIN32)
     LARGE_INTEGER start_count;
     LARGE_INTEGER stop_count;
+#else
+    timespec start_count;
+    timespec stop_count;
+#endif
 };
 
 
@@ -60,28 +69,36 @@ Timer<Units>::~Timer()
 template<TimeUnits Units>
 void Timer<Units>::start()
 {
+#if defined(_WIN32)
     DWORD_PTR prev_am = ::SetThreadAffinityMask(::GetCurrentThread(), 0);
-
     ::QueryPerformanceCounter(&start_count);
-
     ::SetThreadAffinityMask(::GetCurrentThread(), prev_am);
+#else
+    clock_gettime(CLOCK_MONOTONIC, &start_count);
+#endif
 }
 
 template<TimeUnits Units>
 double Timer<Units>::stop()
 {
+#if defined(_WIN32)
     DWORD_PTR prev_am= ::SetThreadAffinityMask(::GetCurrentThread(), 0);
-
     ::QueryPerformanceCounter(&stop_count);
-
     ::SetThreadAffinityMask(::GetCurrentThread(), prev_am);
 
     return ((stop_count.QuadPart - start_count.QuadPart) / frequency * Units);
+#else
+    // TODO implement rv
+    clock_gettime(CLOCK_MONOTONIC, &start_count);
+
+    return 0;
+#endif
 }
 
 template<TimeUnits Units>
 double Timer<Units>::get_frequency()
 {
+#if defined(_WIN32)
     LARGE_INTEGER freq;
 
     if (!::QueryPerformanceFrequency(&freq))
@@ -89,6 +106,15 @@ double Timer<Units>::get_frequency()
             "[ERROR] QueryPerformanceFrequency() failed; your platform may lack support.");
 
     return freq.QuadPart;
+#else
+    // TODO implement rv
+    timespec freq;
+
+    if (clock_getres(CLOCK_MONOTONIC, &freq))
+        throw std::runtime_error("[ERROR] clock_getres() failed.");
+
+    return 0;
+#endif
 }
 
 template<TimeUnits Units>
